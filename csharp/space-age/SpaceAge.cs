994b89dfb4d825eaeead45450fecc7180baf334b
jeff hart <jeffhart91@gmail.com>
2019-06-27 13:35:02 -0500

    Space Age submission

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -1,48 +1,61 @@
-using System;
-
 public class SpaceAge
 {
+    private readonly double seconds;
+
     public SpaceAge(int seconds)
     {
+        this.seconds = seconds;
     }
 
     public double OnEarth()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return this.seconds / OrbitalPeriod.Earth;
     }
 
     public double OnMercury()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return this.seconds / OrbitalPeriod.Mercury;
     }
 
     public double OnVenus()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return this.seconds / OrbitalPeriod.Venus;
     }
 
     public double OnMars()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return this.seconds / OrbitalPeriod.Mars;
     }
 
     public double OnJupiter()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return this.seconds / OrbitalPeriod.Jupiter;
     }
 
     public double OnSaturn()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return this.seconds / OrbitalPeriod.Saturn;
     }
 
     public double OnUranus()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return this.seconds / OrbitalPeriod.Uranus;
     }
 
     public double OnNeptune()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return this.seconds / OrbitalPeriod.Neptune;
     }
+}
+
+public struct OrbitalPeriod
+{
+    public const double Earth = 31557600;
+    public const double Mercury = Earth * 0.2408467;
+    public const double Venus = Earth * 0.61519726;
+    public const double Mars = Earth * 1.8808158;
+    public const double Jupiter = Earth * 11.862615;
+    public const double Saturn = Earth * 29.447498;
+    public const double Uranus = Earth * 84.016846;
+    public const double Neptune = Earth * 164.79132;
 }
